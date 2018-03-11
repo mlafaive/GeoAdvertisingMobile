c04a05c35f577323f1de383d38579363cde52663
Luis Sosa <sosaluis@umich.edu>
2018-03-11 17:08:42 -0400

    Adding the settings page

--- components/settings/Settings.js
+++ components/settings/Settings.js
@@ -1,23 +1,79 @@
 import React from 'react';
-import { Text, View } from 'react-native';
-
+import { Text, View, TextInput } from 'react-native';
+import { Button, ButtonGroup, Icon } from 'react-native-elements';
 import HeaderView from '../header_view/HeaderView.js';
-
 import PropTypes from 'prop-types';
-
 import styles from './Styles.js';
 
 class Settings extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      menu: false
+      editing: false,
+      name: 'John',
+      email: 'john@gmail.com',
+      password: '123hackme',
+      interests: 'none'
     };
+
+    this.edit = () => {
+      this.setState({
+        editing: !this.state.editing
+      });
+    }
+
+    this.fillText = (type) => {
+      if(this.state.editing)
+      {
+          return (<TextInput 
+            style={styles.inputBox}
+            textAlign='left'
+            autoCapitalize='none'
+            autoCorrect={false}
+            placeholder={this.state[type]}
+            value={this.state[type]}
+            />);
+      }
+      else
+      {
+          return (<Text style={styles.inputText}>{this.state[type]}</Text>);
+      }
   }
+}
   render() {
     return (
         <HeaderView style={styles.container} history={this.props.history}>
-          <Text>Settings</Text>
+          <View style={styles.screen}>
+
+
+            <View style={styles.settingsRow} >
+              <Text style={styles.blank} > </Text>
+              <Text style={styles.settings}>Settings</Text>
+              <Button 
+                  borderRadius={5}
+                  buttonStyle={styles.button}
+                  textStyle={styles.buttonText}
+                  title={this.state.editing ? 'Save' : 'Edit '}
+                  onPress={this.edit}
+              />
+            </View>
+            <View style={styles.rowsView}> 
+              <Text style={styles.rows}>Name: </Text>
+              { this.fillText('name') }
+            </View>
+            <View style={styles.rowsView}> 
+              <Text style={styles.rows}>Email: </Text>
+              { this.fillText('email') }
+            </View>
+            <View style={styles.rowsView}> 
+              <Text style={styles.rows}>Password: </Text>
+              { this.fillText('password') }
+            </View>
+            <View style={styles.rowsView}> 
+              <Text style={styles.rows}>Interests: </Text>
+              { this.fillText('interests') }
+            </View>
+          </View>
         </HeaderView>
     );
   }
--- components/settings/Styles.js
+++ components/settings/Styles.js
@@ -4,4 +4,56 @@ export default styles = StyleSheet.create({
 	container: {
 		flex: 1,
 	},
+	screen: {
+		paddingTop: 10,
+		backgroundColor: '#ffffff',
+		flex: 1,
+	},
+	settings: {
+		textAlign: 'center',
+		fontWeight: 'bold',
+		fontSize: 25,
+		paddingBottom: 10,
+		flex: 2
+	},
+	settingsRow: {
+		flexDirection: 'row',
+		height: 35
+	},
+	blank: {
+		flex: 1,
+	},
+	rowsView: {
+    	paddingLeft: 25,
+    	height: 35,
+    	flexDirection: 'row',
+	},
+	rows: {
+    	fontSize: 18,
+    	flex:1,
+	},
+	button: {
+		backgroundColor: '#faebd7', 
+    	borderRadius: 5,
+    	height: 30,
+    	flex: 1
+	},
+	buttonText: {
+		color: '#000000',
+	},
+	inputText: {
+		paddingLeft: 10,
+		fontSize: 17,
+		flex:2.5
+	},
+	inputBox: {
+    	backgroundColor: '#FFFFFF',
+    	height: 24,
+    	fontSize: 17,
+    	borderRadius: 10,
+    	borderWidth: 0.5,
+    	borderColor: '#d6d7da',
+    	flex: 2.5,
+    	paddingLeft: 10
+  	},
 });
