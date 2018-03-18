fa692af8b4a392d3cb941e5042e21bf3f08f6d03
mlafaive <mlafaive@umich.edu>
2018-03-18 18:59:51 -0400

    get location function

---
+++ permissions/Permissions.js
@@ -0,0 +1,9 @@
+export async function getLocation() {
+  const { Location, Permissions } = Expo;
+  const { status } = await Permissions.askAsync(Permissions.LOCATION);
+  if (status === 'granted') {
+    return Location.getCurrentPositionAsync({enableHighAccuracy: true});
+  } else {
+    throw new Error('Location permission not granted');
+  }
+}
